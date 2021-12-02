f6e6a82ebb870b53fb799ec34adf9c8ea445e135
axelzedigh <axel.zedigh@gmail.com>
2021-12-02 18:19:26 +0100

    History log function training dataset fix.

--- plots/history_log_plots.py
+++ plots/history_log_plots.py
@@ -8,11 +8,13 @@ from utils.db_utils import get_training_model_file_path
 
 
 def plot_history_log(
+        training_dataset_id: int,
         trace_process_id: int,
         keybyte: int,
         additive_noise_method_id: Optional[int],
         denoising_method_id: Optional[int],
         save: bool = False,
+        show: bool = True,
 ) -> None:
     """
     Plot the history function. Accuracy on the left, loss on the right.
@@ -25,6 +27,7 @@ def plot_history_log(
         epoch=1,
         keybyte=keybyte,
         trace_process_id=trace_process_id,
+        training_dataset_id=training_dataset_id,
     )
     training_path = os.path.dirname(training_file_path)
     history_log_file_path = os.path.join(training_path, "history_log.npy")
@@ -36,17 +39,20 @@ def plot_history_log(
 
     # Subplot 1 - Accuracy
     ax1 = fig.add_axes((0.1, 0.1, 0.35, 0.8))
-    ax1.plot(history_log["accuracy"], solid_capstyle="round", linewidth=2)
-    ax1.plot(history_log["val_accuracy"], solid_capstyle="round")
+    ax1.plot(history_log["accuracy"], solid_capstyle="round", linewidth=2, label="Accuracy")
+    ax1.plot(history_log["val_accuracy"], solid_capstyle="round", label="Validation Accuracy")
     ax1.set_xlabel("Epoch")
     ax1.set_ylabel("Accuracy")
 
     # Subplot 2 - Loss
     ax2 = fig.add_axes((0.55, 0.1, 0.35, 0.8))
-    ax2.plot(history_log["loss"], linewidth=2)
-    ax2.plot(history_log["val_loss"])
+    ax2.plot(history_log["loss"], linewidth=2, label="Loss")
+    ax2.plot(history_log["val_loss"], label="Validation Loss")
     ax2.set_xlabel("Epoch")
     ax2.set_ylabel("Loss")
+    ax1.legend()
+    ax2.legend()
     if save:
         plt.savefig(fname=history_log_fig_file_path)
-    plt.show()
+    if show:
+        plt.show()
