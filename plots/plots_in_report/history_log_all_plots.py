7a7e2fed7813745defa83a8508d9154f316bbb32
= <=>
2022-01-16 18:08:23 +0100

    History log plotting file.

---
+++ plots/plots_in_report/history_log_all_plots.py
@@ -0,0 +1,86 @@
+from plots.history_log_plots import plot_history_log__overview_trace_process
+
+if __name__ == '__main__':
+    plot_history_log__overview_trace_process(
+        training_dataset_id=1,
+        trace_process_id=3,
+        file_format="pgf",
+        show=False,
+        last_gaussian=5,
+        last_collected=9,
+        last_rayleigh=11,
+        nrows=4,
+        ncols=4,
+    )
+
+    plot_history_log__overview_trace_process(
+        training_dataset_id=3,
+        trace_process_id=4,
+        file_format="pgf",
+        show=False,
+        last_gaussian=5,
+        last_collected=9,
+        last_rayleigh=11,
+        nrows=4,
+        ncols=4,
+    )
+
+    plot_history_log__overview_trace_process(
+        training_dataset_id=3,
+        trace_process_id=9,
+        file_format="pgf",
+        show=False,
+        last_gaussian=5,
+        last_collected=9,
+        last_rayleigh=11,
+        nrows=4,
+        ncols=4,
+    )
+
+    plot_history_log__overview_trace_process(
+        training_dataset_id=3,
+        trace_process_id=10,
+        file_format="pgf",
+        show=False,
+        last_gaussian=5,
+        last_collected=9,
+        last_rayleigh=11,
+        nrows=4,
+        ncols=4,
+    )
+
+    plot_history_log__overview_trace_process(
+        training_dataset_id=3,
+        trace_process_id=8,
+        file_format="pgf",
+        show=False,
+        last_gaussian=5,
+        last_collected=9,
+        last_rayleigh=11,
+        nrows=4,
+        ncols=4,
+    )
+
+    plot_history_log__overview_trace_process(
+        training_dataset_id=3,
+        trace_process_id=12,
+        file_format="pgf",
+        show=False,
+        last_gaussian=5,
+        last_collected=9,
+        last_rayleigh=11,
+        nrows=4,
+        ncols=4,
+    )
+
+    plot_history_log__overview_trace_process(
+        training_dataset_id=3,
+        trace_process_id=11,
+        file_format="pgf",
+        show=False,
+        last_gaussian=5,
+        last_collected=9,
+        last_rayleigh=11,
+        nrows=4,
+        ncols=4,
+    )
